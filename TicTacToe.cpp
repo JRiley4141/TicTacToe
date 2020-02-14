cf0d8ef990939078f1c807b514d84c1fe1d17ebf
JRiley4141 <42786690+JRiley4141@users.noreply.github.com>
2020-02-14 12:14:09 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,31 +1,110 @@
-/* Jennifer Riley & Michelle
-    TicTacToe Game
-*/
 #include <iostream>
+#include <string>
+
+#include "TicTacToe.h"
 
 using namespace std;
 
-void DisplayBoard(char array[][3], int size);
-
-char board[3][3] = { { '_','_', '_'}, {'_','_', '_'}, {'_','_', '_'} };
-int size = 3;
-
-
-int main(){
-    DisplayBoard(board, 3);
+TicTacToe::TicTacToe(){
+    for (int r = 0; r < 3; r++) {
+        for (int c = 0; c < 3; c++) {
+            board[r][c] = '_';
+        }
+    }
 }
 
-void DisplayBoard(char array[][3], int size){
+void TicTacToe::CreateBoard(){
+    system("cls");
     cout<< "********** Let's Play Tic Tac Toe **********"<< endl;
-    cout<< "   A    B    C                         "<< endl;
-    for (int row = 0; row < size; row++){
+    
+    for (int row = 0; row < 3; row++){
         cout << row + 1 ;
-        for (int col = 0; col < size; col++){
+        for (int col = 0; col < 3; col++){
             cout << "  " << board[row][col];
             cout << "  ";
         }
         cout << endl;
     }
     cout << endl;
+}
+
+void TicTacToe::GetPlayerChoice(int move){
+    char player = 'X';
+
+    cout <<"\n Enter your move: \n" << endl;
+    cin >> move;
+
     
+    if( move == 1)
+    {
+        board[0][0] = player;
+    }
+    else if(move == 2)
+    {
+        board[0][1] = player;
+    }
+    else if(move == 3)
+    {
+        board[0][2] = player;
+    }
+    else if(move == 4)
+    {
+        board[1][0] = player;
+    }
+    else if(move == 5)
+    {
+        board[1][1] = player;
+    }
+    else if(move == 6)
+    {
+        board[1][2] = player;
+    }
+    else if(move == 7)
+    {
+        board[2][0] = player;
+    }
+    else if(move == 8)
+    {
+        board[2][1] = player;
+    }
+    else if(move == 9)
+    {
+        board[2][2] = player;
+    }
+}
+
+void TicTacToe::PlayerSwitch(char player){
+    if( player == 'X'){
+        player = 'O';
+    }
+    else if (player == 'O'){
+        player = 'X';
+    }
+}
+
+bool TicTacToe::IllegalPlay(){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            if (board[i][j] == 'X' && board[i][j] == 'O'){
+                return true;
+            }
+            else { return false;}
+        }
+    }
+}
+
+
+
+int main(){
+    TicTacToe game;
+    char player = 'X';
+    
+    while(game.IllegalPlay() == false)
+    {
+        game.CreateBoard();
+        game.GetPlayerChoice(player);
+        game.PlayerSwitch(player);
+    }
+    
+
 }
