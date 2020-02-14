c1a2ccf868ae5dfee878a013bbb0be03cc65e325
MichelleTran7803 <42360120+MichelleTran7803@users.noreply.github.com>
2020-02-14 12:27:47 -0700

    Update TicTacToe.cpp
    DisplayBoard fucntion

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -31,10 +31,23 @@ void TicTacToe::CreateBoard(){
     cout << endl;
 }
 
+void DisplayBoard()
+{
+    cout << "---------------------" << endl << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << endl;
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << endl;
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
+    cout << "     |     |     " << endl;
+}
 
 
 int main(){
     TicTacToe game;
     char player = 'X';
     game.CreateBoard();
-  
+    game.Displayboard();  
