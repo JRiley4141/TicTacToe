c163b4c40b2164728da0879156f03fad680368b3
MichelleTran7803 <42360120+MichelleTran7803@users.noreply.github.com>
2020-02-14 12:22:31 -0700

    Update TicTacToe.cpp
    Create Board function is intialized within the constructor

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,30 +2,39 @@
     TicTacToe Game
 */
 #include <iostream>
+#include <string>
+
+#include "TicTacToe.h"
 
 using namespace std;
 
-void DisplayBoard(char array[][3], int size);
-
-char board[3][3] = { { '_','_', '_'}, {'_','_', '_'}, {'_','_', '_'} };
-int size = 3;
-
-
-int main(){
-    DisplayBoard(board, 3);
+TicTacToe::TicTacToe(){
+    for (int r = 0; r < 3; r++) {
+        for (int c = 0; c < 3; c++) {
+            board[r][c] = '_';
+        }
+    }
 }
 
-void DisplayBoard(char array[][3], int size){
+void TicTacToe::CreateBoard(){
+    system("cls");
     cout<< "********** Let's Play Tic Tac Toe **********"<< endl;
-    cout<< "   A    B    C                         "<< endl;
-    for (int row = 0; row < size; row++){
+    
+    for (int row = 0; row < 3; row++){
         cout << row + 1 ;
-        for (int col = 0; col < size; col++){
+        for (int col = 0; col < 3; col++){
             cout << "  " << board[row][col];
             cout << "  ";
         }
         cout << endl;
     }
     cout << endl;
-    
 }
+
+
+
+int main(){
+    TicTacToe game;
+    char player = 'X';
+    game.CreateBoard();
+  
